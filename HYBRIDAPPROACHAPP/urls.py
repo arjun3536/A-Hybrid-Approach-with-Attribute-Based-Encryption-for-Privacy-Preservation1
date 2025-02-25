7d8b9b642e151cd128a308a4c003528f6f56b59d
arjuna <153887720+arjun3536@users.noreply.github.com>
2025-02-25 17:27:10 +0530

    addd

--- HYBRIDAPPROACHAPP/urls.py
+++ HYBRIDAPPROACHAPP/urls.py
@@ -1,30 +1,22 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from HYBRIDAPPROACHAPP import views
+
 urlpatterns = [
-
-    path('',views.loginpage),
-	path('admincourtcasedisplay/',views.admincourtcasedisplay),
-	path('operatorregister',views.operatorregistercode),
-    path('operatordisplay/',views.operatordisplay),
-    path('advocateregister/',views.advocateregistercode),
-    path('adminadvocatedisplay/',views.adminadvocatedisplay),
-    path('adminhome/',views.adminhome),
-    path('operatorhome/',views.operatorhome),
-    path('operatorcourtcasecode/',views.operatorcourtcasecode),
-    path('operatoradvocatedisplay/',views.operatoradvocatedisplay),
-    path('operatorcourtcasedisplay/',views.operatorcourtcasedisplay),
-    path('operatorviewdata/',views.operatorviewdata),
-    path('decryptdata/',views.decryptdata,name="decryptdata"),
-    
-
-
-
-    path('adddata/',views.adddata),
-    path('viewdata/',views.viewdata),
-
-
-    path('advocatehome/',views.advocatehome),
-    path('advocatecourtcasedisplay/',views.advocatecourtcasedisplay),
-    
+    path('', views.loginpage, name="login"),  # Root URL serves login page
+    path('admincourtcasedisplay/', views.admincourtcasedisplay),
+    path('operatorregister/', views.operatorregistercode),
+    path('operatordisplay/', views.operatordisplay),
+    path('advocateregister/', views.advocateregistercode),
+    path('adminadvocatedisplay/', views.adminadvocatedisplay),
+    path('adminhome/', views.adminhome),
+    path('operatorhome/', views.operatorhome),
+    path('operatorcourtcasecode/', views.operatorcourtcasecode),
+    path('operatoradvocatedisplay/', views.operatoradvocatedisplay),
+    path('operatorcourtcasedisplay/', views.operatorcourtcasedisplay),
+    path('operatorviewdata/', views.operatorviewdata),
+    path('decryptdata/', views.decryptdata, name="decryptdata"),
+    path('adddata/', views.adddata),
+    path('viewdata/', views.viewdata),
+    path('advocatehome/', views.advocatehome),
+    path('advocatecourtcasedisplay/', views.advocatecourtcasedisplay),
 ]
